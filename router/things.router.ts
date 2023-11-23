4f6d84eced8be2ea52be1a462dc154cadde8ca2f
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-23 10:14:35 +0100

    features in api and new implements

---
+++ router/things.router.ts
@@ -0,0 +1,12 @@
+import { Router as createRouter } from 'express';
+import { ThingsController } from '../src/controllers/things.controller.js';
+
+export const thingsRouter = createRouter();
+
+const controller = new ThingsController();
+
+thingsRouter.get('/', controller.getAll.bind(controller));
+thingsRouter.get('/:idThing', controller.getById.bind(controller));
+thingsRouter.delete('/:idThing', controller.delete.bind(controller));
+thingsRouter.post('/', controller.create.bind(controller));
+thingsRouter.patch('/:idThing', controller.update.bind(controller));
